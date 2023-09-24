ee89c7cbcae1283d76c8b560f64ea8819f3d1c0b
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-09-24 11:32:43 +0200

    Update Docker

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -23,7 +23,7 @@ require 'resque-status'
 
 Resque.inline = ENV['APP_ENV'] == 'test'
 Resque.redis = Redis.new(
-  host: ENV['REDIS_RESQUE_HOST'] || ENV['REDIS_HOST'] || 'localhost',
+  host: ENV['REDIS_RESQUE_HOST'] || 'localhost',
   timeout: 300.0
 )
 Resque::Plugins::Status::Hash.expire_in = 7.days # In seconds, a too small value remove working or queuing jobs
