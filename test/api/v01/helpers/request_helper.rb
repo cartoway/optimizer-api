e3a93a0b0b33889309ef0e25277653c487f46095
Gwénaël Rault <gwenael@cartoway.com>
2025-09-22 15:04:56 +0200

    Fix asynchronous tests

--- test/api/v01/helpers/request_helper.rb
+++ test/api/v01/helpers/request_helper.rb
@@ -150,44 +150,14 @@ module TestHelper
     JSON.parse(last_response.body)
   end
 
-  def asynchronously(options = {start_worker: false})
+  def asynchronously(_options = {start_worker: false})
     old_config_solve_synchronously = OptimizerWrapper.config[:solve][:synchronously]
     OptimizerWrapper.config[:solve][:synchronously] = false
     old_resque_inline = Resque.inline
     Resque.inline = false
-    if options[:start_worker] && !ENV['DOCKER']
-      pid_worker = Process.spawn({ 'COUNT' => '1', 'QUEUE' => 'DEFAULT' },
-                                 'bundle exec rake resque:workers --trace', pgroup: true) # don't create another shell
-      pgid_worker = Process.getpgid(pid_worker)
-      while `ps -o pgid | grep #{pgid_worker}`.split(/\n/).size < 2
-        puts "#{Time.now} Waiting for the worker to launch"
-        sleep 0.1
-      end
-      puts "#{Time.now} Worker is started"
-      sleep 0.1
-    end
     yield
   ensure
     Resque.inline = old_resque_inline
     OptimizerWrapper.config[:solve][:synchronously] = old_config_solve_synchronously
-    if options[:start_worker] && !ENV['DOCKER'] && pgid_worker
-      # Kill all grandchildren
-      worker_pids = `ps -o pgid,pid | grep #{pgid_worker}`.split(/\n/)
-      worker_pids.collect!{ |i| i.split(' ')[-1].to_i }
-      worker_pids.sort!
-      worker_pids.reverse_each{ |pid|
-        next if pid == pgid_worker
-
-        Process.kill('SIGKILL', pid)
-        Process.detach(pid)
-      }
-      Process.kill('SIGTERM', -pgid_worker) # Kill the process group (this doesn't kill grandchildren)
-      Process.waitpid(-pgid_worker, 0)
-      while `ps -o pgid,pid | grep "#{worker_pids.join('\|')}"`.split(/\n/).any?
-        puts "#{Time.now} Waiting the worker process group #{pgid_worker} to die\n"
-        sleep 0.1
-      end
-      puts "#{Time.now} Worker is killed"
-    end
   end
 end
